1375f4074c817278da86e90a3f43f375819cc822
John Dinh <johnduydinh@gmail.com>
2025-05-14 21:27:09 -0400

    initial commit

---
+++ .zshrc
@@ -0,0 +1,36 @@
+# Path to your Oh My Zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+zstyle ':omz:update' mode auto      # update automatically without asking
+
+plugins=(
+  git
+  yarn
+  z
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  docker
+  node
+  npm
+  command-not-found
+  fzf
+  history
+  aliases
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# Set preferred edito
+export EDITOR='nvim'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+alias ivim="nvim /home/john/.config/nvim/init.vim"
+alias zshrc="nvim ~/.zshrc"
+alias gs="git status"
+alias ls="ls -lhF --color"
+alias lh="ls -lhd .??*"
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+set -e
+
+echo "[*] Adding Neovim Unstable PPA..."
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+
+echo "[*] Updating system..."
+sudo apt update
+sudo apt upgrade -y
+
+echo "[*] Installing base packages..."
+sudo apt install -y \
+  build-essential \
+  curl \
+  git \
+  wget \
+  unzip \
+  zip \
+  software-properties-common \
+  ca-certificates \
+  gnupg \
+  lsb-release \
+  neovim \
+  tmux \
+  zsh
+
+cp ./.zshrc ~/.zshrc
+
+echo "[*] Installing Oh My Zsh..."
+KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "[*] Installing Powerlevel10k Theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# https://github.com/nvm-sh/nvm?tab=readme-ov-file#manual-install
+echo "[*] Installing NVM: Node Version Manager..."
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
